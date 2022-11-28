a1601d56ae5e390c3fb1408316accdc8e804e89f
Saheel Ahmed <saheelahmed7@gmail.com>
2022-11-28 19:04:59 +0400

    API path reverted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://192.168.5.20/api/',
-  mediaPath: "http://192.168.5.20/uploads/"
+  api: 'http://http://192.168.5.20//api/',
+  mediaPath: "http://internal-spsa.eovstg.com/uploads/"
 };
